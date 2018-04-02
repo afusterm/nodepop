21313a160030f206283d03a3e9b81ef688b5a4a7
Alejandro <afusmir@gmail.com>
2018-04-02 10:10:34 +0200

    resuelto el problema de seguridad con ejs

--- app.js
+++ app.js
@@ -31,10 +31,6 @@ require('./models/Anuncio');
 require('./models/Usuario');
 require('./models/PushToken');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cors())
